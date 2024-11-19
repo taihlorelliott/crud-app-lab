5a14631f1d66ba14cef142ef2be1352df5e24d7e
Taihlor Elliott <taihlor.elliott@gmail.com>
2024-11-19 15:31:17 -0500

    mvp

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const dotenv = require('dotenv')
 dotenv.config()
 //require mongoose to work
 const mongoose = require('mongoose')
+// require the method-override package to work
+const methodOverride = require("method-override");
+// require the morgan package to work
+const morgan = require('morgan');
 
 const app = express();
 
@@ -15,12 +19,17 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}`)
   });
+  
 
 //importing the floss model
 const Floss = require("./models/floss.js")
 
 //tell express to expect data
 app.use(express.urlencoded({ extended: false }));
+//the string in this will be a query param
+app.use(methodOverride("_method"));
+//helps with finding bugs sometimes, not 100% necessary
+app.use(morgan("dev"));
 
 //=============== gets =====================
 //home page
@@ -34,13 +43,19 @@ app.get ("/floss/new", async (req, res) => {
 })
 
 //edit page
-app.get ("/edit", async (req, res) => {
-    res.render("floss/edit.ejs")
+app.get ("/floss/:flossId/edit", async (req, res) => {
+    const addedFloss = await Floss.findById(req.params.flossId);
+    res.render('floss/edit.ejs', {
+        floss: addedFloss
+    })
 })
 
 //index page (craft cabinet)
 app.get ("/index", async (req, res) => {
     const allFloss = await Floss.find();
+    await allFloss.sort((a, b) => {
+        return a.flossNum - b.flossNum 
+    })
     res.render("floss/index.ejs", { floss: allFloss })
 })
 
@@ -57,6 +72,20 @@ app.post("/floss", async (req, res) => {
     console.log(req.body);
     await Floss.create(req.body);
     res.redirect("/index")
+})
+
+
+//================= delete =======================
+//delete floss from cabinet
+app.delete("/floss/:flossId", async (req, res) => {
+    await Floss.findByIdAndDelete(req.params.flossId);
+    res.redirect("/index");
+  });
+
+//================= put ======================
+app.put('/floss/:flossId', async (req, res) => {
+    await Floss.findByIdAndUpdate(req.params.flossId, req.body);
+    res.redirect(`/floss/${req.params.flossId}`)
 })
 
 app.listen(3002, () => {
